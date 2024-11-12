22662a65c1d1d462c91cc1fb6d837f4b881ecb9b
KangJS <asdfqaz1996@naver.com>
2024-11-12 13:43:00 +0900

    [Refactor] 가위바위보 관련 모델 및 컨트롤러 삭제, 챔피언 모델에 이미지 필드 추가

--- models/Champion.js
+++ models/Champion.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const ChampionSchema = new mongoose.Schema({
   name: { type: String, required: true },
   title: { type: String, required: true },
+  image: { type: String, required: true },
+  small: { type: String, required: true },
 });
 
 const Champion = mongoose.model("Champion", ChampionSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const userAPI = require("./user_api");
-const gameAPI = require("./game_api");
 const resultAPI = require("./result_api");
 
 router.use("/user", userAPI);
-router.use("/game", gameAPI);
 router.use("/result", resultAPI);
 
 module.exports = router;
